2b74df901314d263d23b94146abb5a6892f3ae49
foyou <1478449280@qq.com>
2021-06-28 22:19:53 +0800

    reset

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # docts
 document translate, read &amp; translate &amp; write
+
+
+## This is a scheduled task
+## 这是一个计划项目
+
---
+++ docts/__init__.py
@@ -0,0 +1,12 @@
+__title__ = 'docts'
+
+__description__ = 'document translate, read & translate & write'
+__url__ = 'https://github.com/foyoux/docts'
+__version__ = '0.0.3'
+__author__ = 'foyou'
+__author_email__ = 'yimi.0822@qq.com'
+__license__ = 'GPL-3.0'
+__copyright__ = f'Copyright 2021 {__author__}'
+__ide__ = 'PyCharm - https://www.jetbrains.com/pycharm/'
+
+
